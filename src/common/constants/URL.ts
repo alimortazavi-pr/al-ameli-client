dad8ef57caeea0545d8fbf849da9df28d36bbe59
Ali Mortazavi <mortazavimaneshali@gmail.com>
2025-01-04 15:45:58 +0330

    fix: change base api url

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -1,3 +1,3 @@
-// export const BASE_API_URL = "https://al-ameli-back.pourhomestore.top";
-export const BASE_API_URL = "http://localhost:7079";
+export const BASE_API_URL = "https://api.alameli.org";
+// export const BASE_API_URL = "http://localhost:7079";
 export const SERVER_BASE_API_URL = "http://al-ameli-back:7079";
