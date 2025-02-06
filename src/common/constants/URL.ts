999367e7e58e3dd9948dc71a5a63fb43485362c5
Ali Mortazavi <mortazavimaneshali@gmail.com>
2025-02-06 16:18:47 +0330

    fix: url

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -1,3 +1,6 @@
-export const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:7079";
-export const SERVER_BASE_API_URL =
-  process.env.SERVER_BASE_API_URL || "http://localhost:7079";
+// export const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:7079";
+// export const SERVER_BASE_API_URL =
+//   process.env.SERVER_BASE_API_URL || "http://localhost:7079";
+
+export const BASE_API_URL = "https://api.alameli.org";
+export const SERVER_BASE_API_URL = "http://al-ameli-back:7079";
