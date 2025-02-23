d6b0df982f8b468c98356b4db50730101d4874ec
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-02-23 10:54:07 +0330

    fix: page title

--- src/components/providers/Apps/PageTitle.tsx
+++ src/components/providers/Apps/PageTitle.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 //Redux
 import { useAppSelector } from "@/lib/hooks";
 import { pageTitleSelector } from "@/lib/layouts/selectors";
@@ -8,9 +10,16 @@ export const PageTitle = () => {
   //Redux
   const pageTitle = useAppSelector(pageTitleSelector);
 
+  //Next
+  const pathname = usePathname();
+
   return (
-    <div className="bg-secondary py-1 px-2 lg:py-2 lg:px-4 rounded-full">
-      <h3 className="text-base lg:text-3xl font-semibold text-primary-800">{pageTitle}</h3>
-    </div>
+    (pathname.match(/\//g) || []).length === 1 && (
+      <div className="bg-secondary py-1 px-2 lg:py-2 lg:px-4 rounded-full">
+        <h3 className="text-base lg:text-3xl font-semibold text-primary-800">
+          {pageTitle}
+        </h3>
+      </div>
+    )
   );
 };
