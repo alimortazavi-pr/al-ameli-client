0321185bc05a0171b9926b7f28f347ce7aff24c8
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-04-13 17:23:10 +0330

    _

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -3,5 +3,5 @@
 //   process.env.SERVER_BASE_API_URL || "http://localhost:7079";
 
 export const BASE_API_URL = "https://api.alameli.org";
-export const SERVER_BASE_API_URL = "https://api.alameli.org";
-// export const SERVER_BASE_API_URL = "http://al-ameli-back:7079";
+// export const SERVER_BASE_API_URL = "https://api.alameli.org";
+export const SERVER_BASE_API_URL = "http://al-ameli-back:7079";
