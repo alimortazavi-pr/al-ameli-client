8d8c5d6b0937b6c20ffca3b53f50b8871ccc25ae
Ali Mortazavi <alimortazavi.pr@gmail.com>
2025-04-15 21:25:19 +0330

    _

--- src/components/providers/Apps/PageTitle.tsx
+++ src/components/providers/Apps/PageTitle.tsx
@@ -14,7 +14,8 @@ export const PageTitle = () => {
   const pathname = usePathname();
 
   return (
-    (pathname.match(/\//g) || []).length === 1 && (
+    (pathname.match(/\//g) || []).length === 1 &&
+    pageTitle && (
       <div className="bg-secondary py-1 px-2 lg:py-2 lg:px-4 rounded-full">
         <h3 className="text-base lg:text-3xl font-semibold text-primary-800">
           {pageTitle}
